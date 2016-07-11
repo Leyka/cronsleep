dde890570e564f7c4981700a51a58761e29d8087
Skander <skander.kc@gmail.com>
2016-07-11 12:12:31 -0400

    Update caffeine.sh

--- caffeine.sh
+++ caffeine.sh
@@ -3,6 +3,6 @@
 hour="$(date '+%H')"
 # Between 7am and 11pm
 while [ $hour -ge 7 -a $hour -lt 23 ] ; do
-	xdotool key T # Simulate shift key
+	xdotool key shift # Simulate shift key
 	sleep 5 #seconds
 done
